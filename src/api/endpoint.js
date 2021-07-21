c42074c731839570fe70cea82a07b3c0ce7fd9e2
cmur85 <39341418+cmurestudillos@users.noreply.github.com>
2021-07-21 08:15:30 +0200

    Delete src directory

--- src/api/endpoint.js
+++
@@ -1,8 +0,0 @@
-//----------------------------------------------------------------------//
-// Configuracion global de nuestra aplicacion                           //
-//----------------------------------------------------------------------//
-var url = { 
-    endpoint: "https://restcountries.eu/rest/v2/lang/es"
-}
-
-export default url;
